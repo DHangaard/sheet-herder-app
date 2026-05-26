ca8281091fb4cef6f5162c7880762230cbe3bc7b
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-05-27 00:26:51 +0200

    chore(AppRoutes): wire Login and Register - implement routes

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,21 +1,23 @@
 import { Routes, Route } from 'react-router'
 import App from '../App.jsx'
 import Homepage from '../pages/homepage/Homepage.jsx'
+import Login from '../pages/user/login/Login.jsx'
+import Register from '../pages/user/register/Register.jsx'
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<App />}>
                 <Route index element={<Homepage />} />
-                
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
             </Route>
         </Routes>
     )
 }
 
 /*
-import Login from '../pages/user/login/Login.jsx'
-import Register from '../pages/user/register/Register.jsx'
+
 import Account from '../pages/user/account/Account.jsx'
 import CharacterOverview from '../pages/character/overview/CharacterOverview.jsx'
 import CharacterDetail from '../pages/character/detail/CharacterDetail.jsx'
@@ -23,8 +25,6 @@ import CharacterCreate from '../pages/character/create/CharacterCreate.jsx'
 import CampaignOverview from '../pages/campaign/overview/CampaignOverview.jsx'
 import NotFound from '../pages/notFound/NotFound.jsx'
 
-<Route path="login" element={<Login />} />
-<Route path="register" element={<Register />} />
 <Route path="account" element={<Account />} />
 <Route path="characters" element={<CharacterOverview />} />
 <Route path="characters/:id" element={<CharacterDetail />} />
