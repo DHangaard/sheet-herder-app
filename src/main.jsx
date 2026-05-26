21f000cda6905a3828957d2079ce6cc7fb2706af
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-05-27 00:16:24 +0200

    chore(main): update import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
-import { AuthProvider } from './context/AuthContext'
+import AuthProvider from './context/auth/AuthProvider'
 import AppRoutes from './components/AppRoutes'
 
 createRoot(document.getElementById('root')).render(
